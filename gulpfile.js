b8b7d82cf5b03d159b67a02582485d3ba76e79f5
Guillaume <guillaume.latour@gmail.com>
2015-11-02 23:36:17 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+var gulp = require('gulp'),
+  plugins = require('gulp-load-plugins')(),
+  bowerFiles = require('main-bower-files'),
+  connect = require('gulp-connect'),
+  watch = require('gulp-watch');
+
+var paths = {
+  src: './src',
+  dist: './dist'
+}
+
+var pipes = {};
+
+pipes.buildVendorsScripts = function() {
+  var vendorsJs = gulp.src(bowerFiles('**/*.js'))
+    .pipe(plugins.concat('vendors.js'))
+    .pipe(plugins.uglify())
+    .pipe(plugins.rename('vendors.min.js'))
+    .pipe(gulp.dest(paths.dist));
+
+  return vendorsJs;
+}
+
+pipes.buildAppScripts = function() {
+
+  var appJs = gulp.src(paths.src + '/*.js')
+    .pipe(plugins.concat('yt-query-token.js'))
+    .pipe(plugins.uglify())
+    .pipe(plugins.rename('yt-query-token.min.js'))
+    .pipe(gulp.dest(paths.dist));
+
+  return appJs;
+
+
+  // var htmlJsTemplates = gulp.src(paths.src + '/**/*.html')
+  //   .pipe(plugins.angularTemplatecache('html-templates.js', {
+  //     root: '/dist/app'
+  //   }));
+
+  // var vendorsJs = gulp.src(paths.src + '/**/*.html')
+  //   .pipe(plugins.concat('vendors.js'))
+  //   .pipe(plugins.uglify())
+  //   .pipe(plugins.rename('vendors.min.js'))
+  //   .pipe(gulp.dest(paths.dist));
+
+  // return vendorsJs;
+}
+
+pipes.build = function() {
+  var self = {};
+  self.vendorsScripts = pipes.buildVendorsScripts();
+  self.buildAppScripts = pipes.buildAppScripts();
+  // self.appHtml = pipes.buildAppHtml();
+  // self.appScripts = pipes.buildAppScripts();
+  // self.appCss = pipes.buildAppCss();
+
+  // return gulp.src(paths.index)
+  //   .pipe(plugins.inject(self.vendorsScripts, {
+  //     ignorePath: '/public',
+  //     name: 'bower'
+  //   }))
+  //   .pipe(plugins.inject(es.merge(self.appScripts, self.appCss), {
+  //     ignorePath: '/public'
+  //   }))
+  //   .pipe(gulp.dest('./server/views'))
+  //   .pipe(plugins.notify('build completed!'));
+}
+
+
+gulp.task('connect', function() {
+  connect.server({
+    port: 9292,
+    root: 'src'
+  });
+});
+
+gulp.task('html', function() {
+  gulp.src('./*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  plugins.livereload.listen();
+
+  plugins.watch([paths.src + '/**/*.html'], function() {
+    pipes.build();
+    plugins.livereload();
+  });
+  plugins.watch([paths.src + '/*.js'], ['html']);
+});
+
+gulp.task('default', ['connect', 'watch']);
---
+++ src/query.js
@@ -0,0 +1,109 @@
+(function() {
+  angular
+    .module('app')
+    .directive('query', function() {
+      return {
+        restrict: 'E',
+        template: '<div class="query-input">' +
+          '<div ng-repeat="token in _tokens">' +
+          '<token token="token"></token>' +
+          '</div>' +
+          '</div>' +
+          '<div>' +
+          '{{searchQuery}}' +
+          '</div>',
+        controller: queryController,
+        scope: {
+          tokens: '='
+        },
+        link: function(scope, ele, attrs, scope) {
+          var queryInput = $(ele[0]).children()[0];
+          $(ele)
+            .asEventStream('click')
+            .filter(function(it) {
+              return queryInput === it.target;
+            })
+            .onValue(function(it) {
+              for (var i = scope.childScopes.length - 1; i >= 0; i--) {
+                var handled = scope.childScopes[i].selectIfEmpty();
+                if (handled) return true;
+              };
+              return false;
+            });
+        }
+      }
+    });
+
+  function queryController($scope) {
+    $scope._tokens = [new Token(0)];
+    $scope.tokens = [];
+    this.childScopes = [];
+
+    this.addToken = function(manuallyAdded) {
+      if ($scope._tokens[$scope._tokens.length - 1].key != '' && $scope._tokens[$scope._tokens.length - 1].value != '') {
+        var token = new Token($scope._tokens.length);
+        token.manuallyAdded = manuallyAdded;
+        $scope._tokens.push(token);
+      }
+    }
+
+    this.selectNextToken = function(token) {
+      for (var i = this.childScopes.length - 1; i >= 0; i--) {
+        var handled = this.childScopes[i].selectIfEmpty();
+        if (handled) return true;
+      };
+      return false;
+    }
+
+    this.removeToken = function(token) {
+      Bacon
+        .fromArray($scope._tokens)
+        .filter(function(it) {
+          return it !== token.token;
+        }).scan([], function(a, b) {
+          a.push(b);
+          return a;
+        }).digest($scope, '_tokens');
+    }
+
+    this.registerToken = function(tokenDirective) {
+      this.childScopes.push(tokenDirective);
+    }
+
+    Bacon
+      .fromBinder(function(sink) {
+        $scope.$watchAsProperty('_tokens', true)
+          .onValue(function(it) {
+            var filteredArray = Bacon
+              .fromArray(it)
+              .filter(function(it) {
+                return it.key !== '' && it.value !== '' && it.value !== '&nbsp;';
+              });
+
+            filteredArray
+              .map(function(it) {
+                return {
+                  key: it.key,
+                  value: it.value
+                }
+              })
+              .digest($scope, 'tokens');
+
+            filteredArray.scan('', function(a, b) {
+              if (a !== '')
+                a += ' and ';
+              return a += b.key + ' is ' + b.value;
+            }).onValue(function(it) {
+              sink(it);
+            });
+          })
+      })
+      .digest($scope, 'searchQuery');
+  }
+
+  function Token(id) {
+    this.id = id;
+    this.key = '';
+    this.value = '';
+  }
+})();
